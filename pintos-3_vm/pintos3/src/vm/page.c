fb0dd28298052f5cc4a9e9d15879f711e0e8fd49
Jay <huaiyu11@126.com>
2020-12-22 00:53:20 +0800

    stack growth

--- pintos-3_vm/pintos3/src/vm/page.c
+++ pintos-3_vm/pintos3/src/vm/page.c
@@ -52,8 +52,11 @@ page_for_addr (const void *address)
 
       /* No page.  Expand stack? */
 
-/* add code */
-
+      /* add code */
+      if ((p.addr > PHYS_BASE - STACK_MAX) && ((void *)thread_current()->user_esp - 32 < address))
+      {
+          return page_allocate (p.addr, false);
+      }
     }
   return NULL;
 }
